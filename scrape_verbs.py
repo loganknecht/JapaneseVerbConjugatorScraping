4a6f5f7f5549fcf51a751a76537886b56cfcee7c
Hugbot <loganknecht@gmail.com>
2015-12-31 05:02:00 -0800

    Tweaks to variable names and management.

--- scrape_verbs.py
+++ scrape_verbs.py
@@ -34,20 +34,25 @@ def generate_verb_forms():
         #----------------------------
         # Basic Verb Info Here
         #----------------------------
-        class_row = selected_element.xpath("tr[1]/td")[0]
-        stem_row = selected_element.xpath("tr[2]/td")[0]
-        te_form_row = selected_element.xpath("tr[3]/td")[0]
-        infinitive_row = selected_element.xpath("tr[4]/td")[0]
+        class_element = selected_element.xpath("tr[1]/td")[0]
+        stem_element = selected_element.xpath("tr[2]/td")[0]
+        te_form_element = selected_element.xpath("tr[3]/td")[0]
+        infinitive_element = selected_element.xpath("tr[4]/td")[0]
 
-        # print html.tostring(class_row)
-        # print html.tostring(stem_row)
-        # print html.tostring(te_form_row)
-        # print html.tostring(infinitive_row)
+        verb_class = class_element.text.strip()
+        stem = stem_element.text.strip()
+        te_form = te_form_element.text.strip()
+        infinitive = infinitive_element.text.strip()
 
-        # print class_row.text
-        # print stem_row.text
-        # print te_form_row.text
-        # print infinitive_row.text
+        # print html.tostring(class_element)
+        # print html.tostring(stem_element)
+        # print html.tostring(te_form_element )
+        # print html.tostring(infinitive_element)
+
+        # print "Class: {}".format(verb_class)
+        # print "Stem: {}".format(stem)
+        # print "Te Form: {}".format(te_form)
+        # print "Infinitive: {}".format(infinitive)
 
         #----------------------------
         # Verb Form Info Here
@@ -78,9 +83,9 @@ def generate_verb_forms():
             negative_form = negative_form_element.text.strip(
             ) if negative_form_element.text else ""
 
-            print "keigo form: {}".format(keigo_form)
-            print u"Positive: {} Negative: {}".format(positive_form,
-                                                      negative_form)
+            # print "Keigo form: {}".format(keigo_form)
+            # print u"Positive: {} Negative: {}".format(positive_form,
+            #                                           negative_form)
 
 
 def main():
