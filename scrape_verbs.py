e739db36e3fc9f051d8039b5b4d1a4fc5f49cd0b
Hugbot <loganknecht@gmail.com>
2015-12-31 18:45:32 -0800

    Outputing to csv file now. The rows being output don't match the header information. Need to make it so it outputs a column for each information set being tracked.

--- scrape_verbs.py
+++ scrape_verbs.py
@@ -1,58 +1,62 @@
 #!env/bin/python
 # Python Standard Libraries
 import csv
+import re
 # Third-Party Libraries
 from lxml import html
+import os
 import requests
+import unicodecsv as csv
 # Custom Libraries
 # N/A
 
 
 def generate_verb_forms():
+    verb_row_output = []
+    dictionary_form = "Iku"
+
     url = ("http"                               # protocol
            "://www.japaneseverbconjugator.com"  # hostname
            "/VerbDetails.asp"                   # path
-           "?txtVerb=iku&Go=Conjugate")         # query string
+           "?txtVerb=" + dictionary_form + ""   # query string
+           "&Go=Conjugate")
+
     output_file_name = "output.csv"
+    output_file_path = os.path.join(os.getcwd(), "output", output_file_name)
+
     webpage = requests.get(url)
-    # print webpage
-    # print webpage.content
 
     tree_root = html.fromstring(webpage.content)
 
+    # Gets the root table that all the information is in
     xpath = "/html/body/div[2]/div[1]/div[4]/div[1]/table[1]"
-    # xpath =
-    # "/html/body/div[2]/div[1]/div[4]/div[1]/table[1]/tbody[1]/tr[2]/td[3]"
 
     selected_elements = tree_root.xpath(xpath)
-    # print selected_elements
     for selected_element in selected_elements:
-        # print "Element: {}".format(selected_element)
-        # print selected_element.text
-        # print selected_element.xpath("//span[@class='JScript']")[0].text
-
         #----------------------------
         # Basic Verb Info Here
         #----------------------------
         class_element = selected_element.xpath("tr[1]/td")[0]
-        stem_element = selected_element.xpath("tr[2]/td")[0]
-        te_form_element = selected_element.xpath("tr[3]/td")[0]
-        infinitive_element = selected_element.xpath("tr[4]/td")[0]
-
         verb_class = class_element.text.strip()
+        verb_class_row = [u"" + dictionary_form + " Class", verb_class]
+
+        stem_element = selected_element.xpath("tr[2]/td")[0]
         stem = stem_element.text.strip()
+        stem_row = [u"" + dictionary_form + " Stem", stem]
+
+        te_form_element = selected_element.xpath("tr[3]/td")[0]
         te_form = te_form_element.text.strip()
+        te_row = [u"" + dictionary_form + " Te Form", te_form]
+
+        infinitive_element = selected_element.xpath("tr[4]/td")[0]
         infinitive = infinitive_element.text.strip()
+        infinitive_row = [u"" + dictionary_form + " Infinitive", infinitive]
 
-        # print html.tostring(class_element)
-        # print html.tostring(stem_element)
-        # print html.tostring(te_form_element )
-        # print html.tostring(infinitive_element)
-
-        # print "Class: {}".format(verb_class)
-        # print "Stem: {}".format(stem)
-        # print "Te Form: {}".format(te_form)
-        # print "Infinitive: {}".format(infinitive)
+        # Add rows to list
+        verb_row_output.append(verb_class_row)
+        verb_row_output.append(stem_row)
+        verb_row_output.append(te_row)
+        verb_row_output.append(infinitive_row)
 
         #----------------------------
         # Verb Form Info Here
@@ -60,14 +64,34 @@ def generate_verb_forms():
         tbody = selected_element.xpath("tbody")[0]
         verb_form_rows = tbody.xpath("tr")
 
-        # print html.tostring(verb_form_rows)
-        # print verb_form_rows.text
-
-        # Removes top header row
+        # Removes top header row, it's trash and provides bad data
         verb_form_rows = verb_form_rows[1:]
+        current_verb_form = None
         for verb_form_row in verb_form_rows:
 
             last_table_cell_index = len(verb_form_row)
+
+            if last_table_cell_index == 4:
+                current_verb_form_element = verb_form_row.xpath("td[1]")
+                for t in current_verb_form_element:
+                    element_markup = html.tostring(t)
+
+                    verb_form_regex = re.compile("</button>[.\s]*(.*)[.\s]*</td>",
+                                                 re.MULTILINE | re.IGNORECASE)
+                    verb_forms = verb_form_regex.findall(element_markup)
+
+                    current_verb_form = verb_forms[0]
+                    current_verb_form = current_verb_form.strip()
+
+                    #--------------
+                    # Debugging
+                    #--------------
+                    # print current_verb_form
+                    # print element_markup
+                    # print verb_forms
+                    # print t.tag
+                    # print t.tail
+                    # print t.text
 
             keigo_form_element = verb_form_row.xpath(
                 "td[" + str(last_table_cell_index - 2) + "]")[0]
@@ -83,9 +107,40 @@ def generate_verb_forms():
             negative_form = negative_form_element.text.strip(
             ) if negative_form_element.text else ""
 
+            positive_verb_classification = dictionary_form + \
+                " " + current_verb_form + " Positive " + keigo_form
+            negative_verb_classification = dictionary_form + \
+                " " + current_verb_form + " Negative " + keigo_form
+
+            positive_row_output = [u"{}".format(positive_verb_classification),
+                                   u"{}".format(positive_form)]
+
+            negative_row_output = [u"{}".format(negative_verb_classification),
+                                   u"{}".format(negative_form)]
+
+            verb_row_output.append(positive_row_output)
+            verb_row_output.append(negative_row_output)
+
+            # positive_verb_classification = current_verb_form + " Positive " + keigo_form
+            # negative_verb_classification = current_verb_form + " Negative " +
+            # keigo_form
+
+            # print "--------------------------------"
+            # print "Verb Form: {}".format(current_verb_form)
             # print "Keigo form: {}".format(keigo_form)
-            # print u"Positive: {} Negative: {}".format(positive_form,
-            #                                           negative_form)
+            # print u"{} - {}".format(positive_verb_classification,
+            #                         positive_form)
+            # print u"{} - {}".format(negative_verb_classification,
+            #                         negative_form)
+
+        with open(output_file_path, "w") as csv_file_handle:
+            verb_csv_writer = csv.writer(csv_file_handle)
+
+            verb_csv_writer.writerow(["Dictionary Form",
+                                      "Verb Form",
+                                      "Keigo Form",
+                                      "Verb Tone"])
+            verb_csv_writer.writerows(verb_row_output)
 
 
 def main():
