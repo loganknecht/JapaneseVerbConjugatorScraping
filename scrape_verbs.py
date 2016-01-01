40c9f357f5c557b1694ce8a587c8c9e6a597f20f
Hugbot <loganknecht@gmail.com>
2016-01-01 04:44:25 -0800

    Only committing because japanese is being output to file now. However there is a bug where it wont output the correct amount of verbs when there are two versions of one of the keigo forms. For iku this means the presumptive/volitional plain forms are showing as a combined list and instead needs to be separated.

--- scrape_verbs.py
+++ scrape_verbs.py
@@ -7,8 +7,11 @@ from lxml import html
 import os
 import requests
 import unicodecsv as csv
+import HTMLParser
 # Custom Libraries
 # N/A
+
+htmlParser = HTMLParser.HTMLParser()
 
 
 def generate_verb_forms():
@@ -26,7 +29,8 @@ def generate_verb_forms():
 
     webpage = requests.get(url)
 
-    tree_root = html.fromstring(webpage.content)
+    parser = html.HTMLParser(encoding='utf-8')
+    tree_root = html.fromstring(webpage.content, parser=parser)
 
     # Gets the root table that all the information is in
     xpath = "/html/body/div[2]/div[1]/div[4]/div[1]/table[1]"
@@ -53,10 +57,10 @@ def generate_verb_forms():
         infinitive_row = [u"" + dictionary_form + " Infinitive", infinitive]
 
         # Add rows to list
-        verb_row_output.append(verb_class_row)
-        verb_row_output.append(stem_row)
-        verb_row_output.append(te_row)
-        verb_row_output.append(infinitive_row)
+        # verb_row_output.append(verb_class_row)
+        # verb_row_output.append(stem_row)
+        # verb_row_output.append(te_row)
+        # verb_row_output.append(infinitive_row)
 
         #----------------------------
         # Verb Form Info Here
@@ -82,6 +86,7 @@ def generate_verb_forms():
 
                     current_verb_form = verb_forms[0]
                     current_verb_form = current_verb_form.strip()
+                    current_verb_form = dictionary_form + " " + current_verb_form
 
                     #--------------
                     # Debugging
@@ -93,53 +98,70 @@ def generate_verb_forms():
                     # print t.tail
                     # print t.text
 
-            keigo_form_element = verb_form_row.xpath(
+            keigo_form_elements = verb_form_row.xpath(
                 "td[" + str(last_table_cell_index - 2) + "]")[0]
-            positive_form_element = verb_form_row.xpath(
+            keigo_form = keigo_form_elements.text.strip(
+            ) if keigo_form_elements.text else ""
+
+            positive_form_elements = verb_form_row.xpath(
                 "td[" + str(last_table_cell_index - 1) + "]")[0]
-            negative_form_element = verb_form_row.xpath(
+            # positive_form = positive_form_elements.text.strip(
+            # ) if positive_form_elements.text else ""
+            positive_form = "/".join([positive_form_element.text
+                                      for positive_form_element
+                                      in positive_form_elements])
+            print positive_form
+
+            positive_kanji_elements = positive_form_elements.xpath(
+                "span")
+            positive_kanji = "/".join([positive_kanji_element.text
+                                       for positive_kanji_element
+                                       in positive_kanji_elements])
+            # for positive_kanji_element in positive_kanji_elements:
+            #     positive_kanji += positive_kanji_element.text
+
+            negative_form_elements = verb_form_row.xpath(
                 "td[" + str(last_table_cell_index) + "]")[0]
+            negative_form = negative_form_elements.text.strip(
+            ) if negative_form_elements.text else ""
 
-            keigo_form = keigo_form_element.text.strip(
-            ) if positive_form_element.text else ""
-            positive_form = positive_form_element.text.strip(
-            ) if positive_form_element.text else ""
-            negative_form = negative_form_element.text.strip(
-            ) if negative_form_element.text else ""
+            negative_kanji_elements = negative_form_elements.xpath(
+                "span")
+            negative_kanji = ""
+            for negative_kanji_element in negative_kanji_elements:
+                negative_kanji = negative_kanji_element.text
 
-            positive_verb_classification = dictionary_form + \
-                " " + current_verb_form + " Positive " + keigo_form
-            negative_verb_classification = dictionary_form + \
-                " " + current_verb_form + " Negative " + keigo_form
+            positive_row_output = [
+                positive_form,
+                current_verb_form,
+                positive_kanji,
+                "Positive",
+                keigo_form,
+                dictionary_form,
+            ]
 
-            positive_row_output = [u"{}".format(positive_verb_classification),
-                                   u"{}".format(positive_form)]
-
-            negative_row_output = [u"{}".format(negative_verb_classification),
-                                   u"{}".format(negative_form)]
-
+            negative_row_output = [
+                negative_form,
+                current_verb_form,
+                negative_kanji,
+                "Negative",
+                keigo_form,
+                dictionary_form,
+            ]
+            #-----
             verb_row_output.append(positive_row_output)
             verb_row_output.append(negative_row_output)
-
-            # positive_verb_classification = current_verb_form + " Positive " + keigo_form
-            # negative_verb_classification = current_verb_form + " Negative " +
-            # keigo_form
-
-            # print "--------------------------------"
-            # print "Verb Form: {}".format(current_verb_form)
-            # print "Keigo form: {}".format(keigo_form)
-            # print u"{} - {}".format(positive_verb_classification,
-            #                         positive_form)
-            # print u"{} - {}".format(negative_verb_classification,
-            #                         negative_form)
 
         with open(output_file_path, "w") as csv_file_handle:
             verb_csv_writer = csv.writer(csv_file_handle)
 
-            verb_csv_writer.writerow(["Dictionary Form",
-                                      "Verb Form",
-                                      "Keigo Form",
-                                      "Verb Tone"])
+            # verb_csv_writer.writerow([
+            #     "Verb",
+            #     "Verb Form",
+            #     "Verb Tone",
+            #     "Keigo Form",
+            #     "Dictionary Form",
+            # ])
             verb_csv_writer.writerows(verb_row_output)
 
 
